e114de66404cd931634fefaf6f1f3f0dd2e4d753
xavi <xaviw@foxmail.com>
2023-09-07 16:59:02 +0800

    feat: 404页面

--- components/NotFound.vue
+++ components/NotFound.vue
@@ -1,5 +1,25 @@
-<template></template>
+<template>
+  <div class="not-found">
+    <img src="/404.svg" alt="404" class="icon" />
+
+    <VPButton text="返回首页" href="/" style="margin-top: 24px;" />
+  </div>
+</template>
 
 <script setup lang="ts">
-// import * from '../assets/svg/404.svg'
+import { VPButton } from 'vitepress/theme';
 </script>
+
+<style scoped>
+.not-found {
+  height: calc(100vh - var(--vp-nav-height));
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.not-found .icon {
+  height: calc(100% - 160px);
+}
+</style>
